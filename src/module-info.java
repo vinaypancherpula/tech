c41e6020c45f3f4a355a561783812299145627fb
vinay pancherpula <vinayvarmap1999@gmail.com>
2023-06-06 21:26:44 -0700

    vinayvirat

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vihanshi
+ *
+ */
+module vinay {
+}
---
+++ src/vinay/hello.java
@@ -0,0 +1,21 @@
+package vinay;
+
+public class hello {
+
+	public static void main(String[] args) {
+     
+		
+		int num=520;
+		int total=0;
+		for ( int i=0; i<=num; i++) {
+			if ( num%2==0) {
+				total=total + 1;
+
+			}
+			System.out.println(total);
+
+
+		}
+	}
+
+}
